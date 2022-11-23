2e4b2b8029401fef96babe9b2354e47994f030ad
juan.garcia <juan.garcia70@uabc.edu.mx>
2022-11-23 12:15:52 -0800

    Se agrego la interface Table

---
+++ PISYS/PISYS/Table.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PISYS
+{
+    public interface Table<T, K>
+    {
+        //T -> el tipo de dato del modelo
+        //K -> el tipo de dato de la llave primaria
+
+        //Inserta la información en la tabla de la base de datos
+        void Insertar(T obj);
+        //Modifica la información de una tupla en una tabla
+        void Modifificar(T obj);
+        //Elimina la información de una tupla en una tabla
+        void Eliminar(T obj);
+        //Obtiene todas la tuplas de una tabla en una tabla
+        List<T> obtenerTodos();
+        //Obtienen una tupla en especifico
+        T obtener(K id);
+        
+    }
+}
