ee64b01eba062207e74a9482ce18c6f024dd6ead
KevynPalacios <106296055+KevynPalacios@users.noreply.github.com>
2022-12-02 17:15:10 -0800

    Add files via upload
    Ejemplo de PISYS UI con Krypton

---
+++ Ejemplo Krypton/Ejemplo Krypton/PlantillaDeFormulario.cs
@@ -0,0 +1,18 @@
+﻿using ComponentFactory.Krypton.Toolkit;
+using System;
+
+namespace Ejemplo_Krypton
+{
+    public partial class PlantillaDeFormulario : KryptonForm
+    {
+        public PlantillaDeFormulario()
+        {
+            InitializeComponent();
+        }
+
+        private void PlantillaDeFormulario_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
